144690c649f30d36249d486312dbd700683f5217
Chris Kim <chrisdohyunkim@gmail.com>
2025-09-12 19:28:56 +0000

    Initial commit

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby "3.2.1"
 
 gem "sinatra"
 gem "sinatra-contrib"
+gem "ai-chat"
+gem "amazing_print"
 
 # Use Puma as the app server
 gem "puma", "~> 5.0"
--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
+require "ai/chat"
+require "dotenv/load"
+require "amazing_print"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get "/" do
+  erb(:index)
+end
+
+post "/chat" do
+  user_message = params[:message]
+
+  c = AI::Chat.new
+  c.add("You are a compassionate mental health therapist who is good at active listening and pointing people towards the right resources for mental health issues", role: "system")
+  c.add(user_message, role: "user")
+
+  x = c.generate!
+
+  @bot_reply = x
+
+  erb(:chat)
 end
