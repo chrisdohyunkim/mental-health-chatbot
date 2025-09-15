cf32fccb62da6dfe963d9154727a46f65f78a020
Chris Kim <chrisdohyunkim@gmail.com>
2025-09-15 16:24:20 +0000

    formatting

--- app.rb
+++ app.rb
@@ -10,9 +10,10 @@ get "/" do
 end
 
 post "/chat" do
-  user_message = params[:message]
 
 c = AI::Chat.new(api_key: ENV["OPENAI_API_KEY"])
+
+user_message = params[:message]
 
 c.add("You are a compassionate mental health therapist who is good at active listening and pointing people towards the right resources for mental health issues", role: "system")
 c.add(user_message, role: "user")
